10536c9d5c417503dd07119510ebc4a1a4c0b632
Akbar Wijaya <151990608+akbarwjyy@users.noreply.github.com>
2025-06-12 10:44:46 +0700

    create Authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 
 //import BrowserRouter dari react router
 import { BrowserRouter } from "react-router-dom";
 
+//import AuthProvider
+import { AuthProvider } from "./context/AuthContext";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <AuthProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </AuthProvider>
 );
